7b51d34c997b79f9855d818648114919f4685b1f
Jaiane dos Santos Andrade <T600352@tokiomarine.com.br>
2023-04-26 14:15:23 -0300

    js filter and style

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const personagem = document.getElementById("idperson");
 const search = document.getElementById("search-btn");
+const reset = document.getElementById("reset");
 const content = document.getElementById("content");
+const resultContent = document.getElementById("result-content");
 const img = document.getElementById("img");
 
 const fetchApi = (value) => {
@@ -13,31 +15,55 @@ const fetchApi = (value) => {
   return url;
 };
 
-const keys = [
-  "name",
-  "status",
-  "species",
-  "gender",
-  "origin",
-  "image",
-  "location",
-  "episode",
-];
+const keys = ["name", "status", "species", "gender", "origin", "location"];
+const renamingKeys = {
+  name: "Nome",
+  status: "status",
+  species: "Espécie",
+  gender: "Gênero",
+  origin: "Planeta de Origem",
+  location: "Localização",
+};
 
 const buildResult = (result) => {
-  const newObject = {};
-  keys
+  return keys
     .map((key) => document.getElementById(key))
     .map((elem) => {
-      elem.checked && (newObject[elem.name] = result[elem.name]);
+      if (elem.checked === true && typeof result[elem.name] !== "object") {
+        const newElement = document.createElement("p");
+        newElement.innerHTML = `${renamingKeys[elem.name]}: ${
+          result[elem.name]
+        }`;
+        content.appendChild(newElement);
+      } else if (
+        (elem.checked === true && elem.name === "location") ||
+        (elem.checked === true && elem.name === "origin")
+      ) {
+        const newElement = document.createElement("p");
+        newElement.innerHTML = `${renamingKeys[elem.name]}: ${
+          result[elem.name].name
+        }`;
+        content.appendChild(newElement);
+      }
     });
-
-  return newObject;
 };
 
 search.addEventListener("click", async (e) => {
   e.preventDefault();
+  if (personagem.value === "") {
+    return (content.innerHTML = "É necessario buscar o personagem pelo ID");
+  }
   const result = await fetchApi(personagem.value);
-  content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`;
-  img.src = `${result.image}`;
+  if (content.firstChild === null) {
+    resultContent.className = "result-content";
+    img.src = `${result.image}`;
+    buildResult(result);
+  } else {
+    content.innerHTML = "";
+    resultContent.className = "result-content";
+    img.src = `${result.image}`;
+    buildResult(result);
+  }
 });
+
+reset.addEventListener("click", () => location.reload());
